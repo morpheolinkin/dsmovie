ef510e0f069b21e8695ee13a8ddf1e7f8a4fc5dd
Jefferson M Silva <jeffersonmedeirosdasilva@gmail.com>
2022-07-20 21:20:15 -0300

    Ajuste para diretório

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/morpheolinkin">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/EstágioIV</p>
                         </div>
                     </a>
                 </div>
